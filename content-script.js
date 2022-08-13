252da63aa17d76716f0a19e0a4ce90d73f865e11
manhole <manholex@gmail.com>
2022-08-13 23:14:08 +0900

    Copy icons are now displayed, when user transitioned from another page.

--- content-script.js
+++ content-script.js
@@ -1,19 +1,35 @@
-const LABEL = '📋';
+const mainScript = () => {
+    const LABEL = '📋';
 
-// 'code' だけではハッシュ以外も該当してしまうので、notで絞る。
-const hashes = document.querySelectorAll('code a:not(.markdown-title)');
-console.log({hashes});
-hashes.forEach(hashNode => {
-    const hashText = hashNode.textContent;
-    const copyNode = document.createElement('button');
-    copyNode.title = 'コピー';
-    copyNode.textContent = LABEL;
-    copyNode.classList.add('___copy_node');
-    hashNode.parentNode.appendChild(copyNode);
-    copyNode.addEventListener('click', async () => {
-        await navigator.clipboard.writeText(hashText);
-        copyNode.textContent = '✔️';
+    // 'code' だけではハッシュ以外も該当してしまうので、notで絞る。
+    const hashes = document.querySelectorAll('code a:not(.markdown-title)');
+    console.log({hashes});
+    hashes.forEach(hashNode => {
+        const hashText = hashNode.textContent;
+        const id = `___copy_hash_id_${hashText}`
+        if (document.getElementById(id)) {
+            console.log('already registered', id);
+            return;
+        }
+        const copyNode = document.createElement('button');
+        copyNode.id = id;
+        copyNode.title = 'copy commit hash';
+        copyNode.textContent = LABEL;
+        copyNode.classList.add('___copy_hash');
+        hashNode.parentNode.appendChild(copyNode);
+        copyNode.addEventListener('click', async () => {
+            await navigator.clipboard.writeText(hashText);
+            copyNode.textContent = '✔️';
+        });
+        copyNode.addEventListener('blur', () => { copyNode.textContent = LABEL; });
+        copyNode.addEventListener('mouseleave', () => { copyNode.textContent = LABEL; });
     });
-    copyNode.addEventListener('blur', () => { copyNode.textContent = LABEL; });
-    copyNode.addEventListener('mouseleave', () => { copyNode.textContent = LABEL; });
+};
+
+mainScript();
+
+const observer = new MutationObserver((mutationList, observer) => {
+    console.log('mutation observer');
+    mainScript();
 });
+observer.observe(document.body, { subtree: true, childList:true, attributes: true });
