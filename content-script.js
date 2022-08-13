9d5d7913d0f1b28783d2727844a7ba44a3222967
manhole <manholex@gmail.com>
2022-08-13 22:44:03 +0900

    click feedback

--- content-script.js
+++ content-script.js
@@ -1,12 +1,19 @@
+const LABEL = '📋';
+
 // 'code' だけではハッシュ以外も該当してしまうので、notで絞る。
 const hashes = document.querySelectorAll('code a:not(.markdown-title)');
 console.log({hashes});
 hashes.forEach(hashNode => {
     const hashText = hashNode.textContent;
-    const child = document.createElement('span');
-    child.textContent = '📋';
-    hashNode.parentNode.appendChild(child);
-    child.addEventListener('click', async () => {
-        navigator.clipboard.writeText(hashText);
+    const copyNode = document.createElement('button');
+    copyNode.title = 'コピー';
+    copyNode.textContent = LABEL;
+    copyNode.classList.add('___copy_node');
+    hashNode.parentNode.appendChild(copyNode);
+    copyNode.addEventListener('click', async () => {
+        await navigator.clipboard.writeText(hashText);
+        copyNode.textContent = '✔️';
     });
+    copyNode.addEventListener('blur', () => { copyNode.textContent = LABEL; });
+    copyNode.addEventListener('mouseleave', () => { copyNode.textContent = LABEL; });
 });
